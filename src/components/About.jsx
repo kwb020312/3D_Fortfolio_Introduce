2a9e5e2969cef19e702fb8dfe64f46e5529329f0
N S <kwb020312@naver.com>
2023-04-11 22:13:45 +0900

    About 페이지 Skill 구성 완료

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -57,4 +58,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
