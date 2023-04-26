258db27c98849c2edc22c685c93fea7a6f0b26ea
김우빈 <46777310+kwb020312@users.noreply.github.com>
2023-04-26 22:57:46 +0900

    Fix-모바일에서 컴퓨터 이동 불가

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -52,11 +52,11 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
+        {!isMobile && <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-        />
+        />}
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
