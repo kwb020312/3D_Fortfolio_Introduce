381904d1e28d683a74e22d7eafd3d16883be4fcf
김우빈 <46777310+kwb020312@users.noreply.github.com>
2023-04-27 12:06:32 +0900

    Fix-Mobile View

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
@@ -13,9 +14,9 @@ const ProjectCard = ({
   tags,
   image,
   source_code_link,
+  isMobile
 }) => {
-  return (
-    // <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+  if(isMobile) return (
     <div>
       <Tilt
         options={{
@@ -58,38 +59,117 @@ const ProjectCard = ({
           ))}
         </div>
       </Tilt>
-    {/* </motion.div> */}
     </div>
+  )
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
   );
 };
 
 const Works = () => {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  if(isMobile) return (
     <>
-      {/* <motion.div variants={textVariant()}> */}
       <div>
         <p className={styles.sectionSubText}>
         개발했던 프로젝트
         </p>
         <h2 className={styles.sectionHeadText}>프로젝트 목록</h2>
       </div>
-      {/* </motion.div> */}
       <div className="w-full flex">
-        {/* <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        > */}
         <p>
           아래 프로젝트들은 진행했던 제 작업들을 나열하여 그간 학습했던 기술과
           경험을 반영합니다. 각 프로젝트들은 깃헙 레포지토리 및 라이브 데모에
           대한 링크와 함께 간략한 설명이 있으며, 복잡한 문제를 해결하고 다양한
           기술을 활용하는 프로젝트를 좋아합니다.
         </p>
-        {/* </motion.p> */}
       </div>
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard key={`project-${index}`} index={index} isMobile={isMobile} {...project} />
+        ))}
+      </div>
+    </>
+  )
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+        개발했던 프로젝트
+        </p>
+        <h2 className={styles.sectionHeadText}>프로젝트 목록</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          아래 프로젝트들은 진행했던 제 작업들을 나열하여 그간 학습했던 기술과
+          경험을 반영합니다. 각 프로젝트들은 깃헙 레포지토리 및 라이브 데모에
+          대한 링크와 함께 간략한 설명이 있으며, 복잡한 문제를 해결하고 다양한
+          기술을 활용하는 프로젝트를 좋아합니다.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} isMobile={isMobile} {...project} />
         ))}
       </div>
     </>
