26eb41283f67375e768552c53499f3391562ffbf
김우빈 <46777310+kwb020312@users.noreply.github.com>
2023-04-27 11:41:22 +0900

    Fix-Computer Resize

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -19,7 +19,7 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.6 : 0.75}
+        scale={isMobile ? 0.4 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1]}
         rotation={[-0.01, -0.2, -0.1]}
       />
